dbd51bc2a7a2d3dd0e3c0ec8b295a048b228281d
Cezary Grzanka <cezarygrzanka@gmail.com>
2023-09-11 16:37:28 +0200

    Autocreate base command

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,3 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
+
+if (prisma) {
+  const count = await prisma.commands.count();
+  if (!count) {
+    await prisma.commands.create({
+      data: {
+        name: "points",
+        enabled: true,
+        message: "$username has $points",
+      },
+    });
+  }
+}
