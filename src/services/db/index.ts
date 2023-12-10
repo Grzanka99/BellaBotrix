a0f324276ad7cb616b237942896f562e729832bb
Cezary Grzanka <cezarygrzanka@gmail.com>
2023-12-10 15:31:56 +0100

    Revert "Migrate to turso" - Base plan won't be enought cause bad optimalization for now
    This reverts commit 2b74b940902ea0b18565c72b5e2eee2fb9e521fb.

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -1,16 +1,7 @@
-import { createClient } from "@libsql/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { PrismaClient } from "@prisma/client";
 import { AsyncQueue } from "utils/async-queue";
 
-const libsql = createClient({
-  url: String(Bun.env.TURSO_DATABASE_URL),
-  authToken: String(Bun.env.TURSO_AUTH_TOKEN),
-});
-
-const adapter = new PrismaLibSQL(libsql);
-
-export const prisma = new PrismaClient({adapter});
+export const prisma = new PrismaClient();
 export const prismaQueue = new AsyncQueue();
 
 if (prisma) {
@@ -18,3 +9,4 @@ if (prisma) {
     await prismaQueue.enqueue(() => prisma.commands.deleteMany());
   }
 }
+
