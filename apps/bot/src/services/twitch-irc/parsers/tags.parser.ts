f399c038ff50a76a89edc7466056fb14844f4843
Cezary Grzanka <cezarygrzanka@gmail.com>
2024-02-03 00:52:48 +0100

    Replace ifs in tags.parser with switch case

--- apps/bot/src/services/twitch-irc/parsers/tags.parser.ts
+++ apps/bot/src/services/twitch-irc/parsers/tags.parser.ts
@@ -4,7 +4,7 @@ import { parseEmotes } from "./emotes.parser";
 
 export function parseTags(data: string, message: string): TTwitchTags {
   const splitted = data.split(";");
-  const userInfo: TTwitchTags = {
+  const tags: TTwitchTags = {
     clientNonce: "",
     color: "",
     displayName: "",
@@ -20,33 +20,45 @@ export function parseTags(data: string, message: string): TTwitchTags {
   };
 
   for (let i = 0; i < splitted.length; i++) {
-    const curr = splitted[i];
-    if (curr.startsWith("badges=")) {
-      userInfo.badges = parseBadges(curr.substring("badges=".length).split(","));
-    } else if (curr.startsWith("client-nonce=")) {
-      userInfo.clientNonce = curr.substring("client-nonce=".length);
-    } else if (curr.startsWith("color=")) {
-      userInfo.color = curr.substring("color=".length);
-    } else if (curr.startsWith("display-name=")) {
-      const uname = curr.substring("display-name=".length);
-      userInfo.displayName = uname;
-      userInfo.username = uname.toLowerCase();
-    } else if (curr.startsWith("emotes=")) {
-      userInfo.emotes = parseEmotes(curr.substring("emotes=".length), message);
-    } else if (curr.startsWith("first-msg=")) {
-      userInfo.firstMessage = curr.substring("first-msg=".length) === "1";
-    } else if (curr.startsWith("mod=")) {
-      userInfo.isMod = curr.substring("mod=".length) === "1";
-    } else if (curr.startsWith("room-id=")) {
-      userInfo.roomId = Number(curr.substring("room-id=".length));
-    } else if (curr.startsWith("subscriber=")) {
-      userInfo.isSubsriber = curr.substring("subscriber=".length) === "1";
-    } else if (curr.startsWith("user-id=")) {
-      userInfo.userId = curr.substring("user-id=".length);
-    } else if (curr.startsWith("user-type=")) {
-      userInfo.userType = curr.substring("user-type=".length);
+    const [tag, value] = splitted[i].split("=");
+
+    switch (tag) {
+      case "badges":
+        tags.badges = parseBadges(value.split(","));
+        break;
+      case "client-nonce":
+        tags.clientNonce = value;
+        break;
+      case "color":
+        tags.color = value;
+        break;
+      case "display-name":
+        tags.username = value.toLowerCase();
+        tags.displayName = value;
+        break;
+      case "emotes":
+        tags.emotes = parseEmotes(value, message);
+        break;
+      case "first-msg":
+        tags.firstMessage = value === "1";
+        break;
+      case "mod":
+        tags.isMod = value === "1";
+        break;
+      case "room-id":
+        tags.roomId = Number(value);
+        break;
+      case "subscriber":
+        tags.isSubsriber = value === "1";
+        break;
+      case "user-id":
+        tags.userId = value;
+        break;
+      case "user-type":
+        tags.userType = value;
+        break;
     }
   }
 
-  return userInfo;
+  return tags;
 }
