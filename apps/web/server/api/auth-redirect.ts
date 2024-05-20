fe75d3d03a27781d07376fecf6feca8c30f2338c
Cezary Grzanka <cezarygrzanka@gmail.com>
2024-05-20 17:33:58 +0200

    Add moderator:read:followers for future purposes

--- apps/web/server/api/auth-redirect.ts
+++ apps/web/server/api/auth-redirect.ts
@@ -3,7 +3,12 @@ export default defineEventHandler(async (event) => {
   const clientId = process.env.API_CLIENT_ID;
   const redirectUri = process.env.APP_URL || "http://localhost:3000";
   const responseType = "code";
-  const scopes = ["moderator:read:chatters", "moderation:read", "channel:moderate"];
+  const scopes = [
+    "moderator:read:chatters",
+    "moderation:read",
+    "channel:moderate",
+    "moderator:read:followers",
+  ];
 
   await sendRedirect(
     event,
